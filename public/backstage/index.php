6513e90cedffd206a9b1e6994d617d01ac211b79
张锞卫 <janggwazkw@gmail.com>
2017-05-29 17:29:51 +0800

    前端页面

---
+++ public/backstage/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chuhangyu
+ * Date: 2017/5/2
+ * Time: 15:17
+ */
+$url = "http://cdn.muyucloud.com/hhxcback1.6";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>/index.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="http://cdn.muyutech.com/common/mdeditor.css" />
+    <title>好好学车后台</title>
+    <!--[if lt IE 10]>
+    <script src="https://as.alipayobjects.com/g/component/??console-polyfill/0.2.2/index.js,es5-shim/4.5.7/es5-shim.min.js,es5-shim/4.5.7/es5-sham.min.js,html5shiv/3.7.2/html5shiv.min.js,media-match/2.0.2/media.match.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div id="react-content"></div>
+</body>
+<script type="text/javascript" src="http://jssdk.demo.qiniu.io/src/qiniu.js"></script>
+<script type="text/javascript" src="http://jssdk.demo.qiniu.io/bower_components/plupload/js/moxie.js"></script>
+<script type="text/javascript" src="http://jssdk.demo.qiniu.io/bower_components/plupload/js/plupload.full.min.js"></script>
+<script type="text/javascript" src="http://jssdk.demo.qiniu.io/bower_components/plupload/js/i18n/zh_CN.js"></script>
+<script src="<?php echo $url; ?>/common.js"></script>
+<script src="<?php echo $url; ?>/index.js"></script>
+</html>
+
