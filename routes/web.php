db25ec0eee2df0bd8cdfca657c3ddbc114f07800
张锞卫 <janggwazkw@gmail.com>
2017-05-29 17:27:59 +0800

    student模块

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('user/login', 'UserController@login');
+Route::post('stu/apply', 'StuController@stuApply');
+Route::post('stu/list', 'StuController@getStuList');
